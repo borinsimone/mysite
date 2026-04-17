cb4e9c932c3a2a5e524dc74f8da579a77f659698
borinsimone <borin.simone1997@gmail.com>
2026-04-17 10:10:26 +0200

    feat: add Contact component and integrate into Home page; update layout and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Sidebar from './components/sidebar/Sidebar';
 import HomePage from './home/Home';
 import About from './about/About';
 import Services from './services/Services';
+import Contact from './contact/Contact';
 export default function Home() {
   return (
     <div className={styles.page}>
@@ -11,6 +12,7 @@ export default function Home() {
         <HomePage />
         <About />
         <Services />
+        <Contact />
       </div>
     </div>
   );
